75857914857fcba1607d2b9b0549b6c68dbe211a
Elouan Grimm <elouan.grimm@gmail.com>
2024-07-21 15:44:59 -0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,48 +1,132 @@
+def game_over():
+    basic.clear_screen()
+    music.play(music.create_sound_expression(WaveShape.SQUARE,
+            5000,
+            0,
+            255,
+            84,
+            1000,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.show_string("" + str(game.score()))
+    ball.delete()
+    item.delete()
+    setup()
+
+def my_function():
+    game.set_score(0)
+buttonClicks.on_button_held(buttonClicks.AorB.B, my_function)
+
+def point():
+    global item
+    game.add_score(1)
+    music.play(music.create_sound_expression(WaveShape.SQUARE,
+            1,
+            5000,
+            198,
+            164,
+            200,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.CURVE),
+        music.PlaybackMode.IN_BACKGROUND)
+    item.delete()
+    item = game.create_sprite(randint(0, 4), randint(0, 4))
+
+def on_button_pressed_ab():
+    basic.show_string("" + str(game.score()))
+    basic.pause(100)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def setup():
+    global sound_on, gameover, started, timermode
+    sound_on = True
+    gameover = False
+    started = False
+    while started != True:
+        if input.button_is_pressed(Button.A):
+            timermode = True
+            started = True
+            music.play(music.create_sound_expression(WaveShape.SQUARE,
+                    147,
+                    5000,
+                    105,
+                    255,
+                    291,
+                    SoundExpressionEffect.NONE,
+                    InterpolationCurve.CURVE),
+                music.PlaybackMode.UNTIL_DONE)
+            start()
+        if input.button_is_pressed(Button.B):
+            timermode = False
+            started = True
+            music.play(music.create_sound_expression(WaveShape.TRIANGLE,
+                    1,
+                    5000,
+                    40,
+                    255,
+                    500,
+                    SoundExpressionEffect.NONE,
+                    InterpolationCurve.LINEAR),
+                music.PlaybackMode.UNTIL_DONE)
+            start()
+        basic.pause(10)
+
+def on_logo_pressed():
+    global sound_on
+    if sound_on:
+        music.set_built_in_speaker_enabled(False)
+        sound_on = False
+    else:
+        music.set_built_in_speaker_enabled(True)
+        sound_on = True
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def start():
+    global ball, item
+    ball = game.create_sprite(2, 2)
+    item = game.create_sprite(randint(0, 4), randint(0, 4))
 timer = 0
+timermode = False
+started = False
 gameover = False
-ball = game.create_sprite(2, 2)
-item = game.create_sprite(randint(0, 4), randint(0, 4))
+sound_on = False
+item: game.LedSprite = None
+ball: game.LedSprite = None
+setup()
 
 def on_forever():
-    global timer, gameover
-    if not (gameover):
-        basic.pause(1000)
-        timer += 1
-        if timer >= 30:
-            ball.delete()
-            item.delete()
-            basic.show_string("" + str(game.score()))
-            basic.pause(100)
-            gameover = True
-            game.set_score(0)
-            basic.clear_screen()
+    if not (ball.is_touching(item)):
+        while not (ball.is_touching(item)):
+            basic.pause(10)
+        if ball.is_touching(item):
+            point()
+        basic.pause(10)
 basic.forever(on_forever)
 
 def on_forever2():
-    if not (gameover):
-        if input.acceleration(Dimension.X) < -200:
-            ball.change(LedSpriteProperty.X, -1)
-            basic.pause(50)
-        if input.acceleration(Dimension.X) > 200:
-            ball.change(LedSpriteProperty.X, 1)
-            basic.pause(50)
-        if input.acceleration(Dimension.Y) < -200:
-            ball.change(LedSpriteProperty.Y, -1)
-            basic.pause(50)
-        if input.acceleration(Dimension.Y) > 200:
-            ball.change(LedSpriteProperty.Y, 1)
-            basic.pause(50)
+    if input.acceleration(Dimension.X) < -200:
+        ball.change(LedSpriteProperty.X, -1)
+        basic.pause(50)
+    if input.acceleration(Dimension.X) > 200:
+        ball.change(LedSpriteProperty.X, 1)
+        basic.pause(50)
+    if input.acceleration(Dimension.Y) < -200:
+        ball.change(LedSpriteProperty.Y, -1)
+        basic.pause(50)
+    if input.acceleration(Dimension.Y) > 200:
+        ball.change(LedSpriteProperty.Y, 1)
+        basic.pause(50)
     basic.pause(10)
 basic.forever(on_forever2)
 
 def on_forever3():
-    global item
-    if not (gameover):
-        while not (ball.is_touching(item)):
-            basic.pause(10)
-        if ball.is_touching(item):
-            game.add_score(1)
-            item.delete()
-            item = game.create_sprite(randint(0, 4), randint(0, 4))
-    basic.pause(10)
+    global timer
+    if timermode == True:
+        timer = 0
+        while timer < 30:
+            basic.pause(1000)
+            timer += 1
+        basic.pause(10)
+        game_over()
 basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,85 +1,122 @@
-function game_over () {
+function game_over() {
     basic.clearScreen()
-    basic.showString("" + (game.score()))
+    music.play(music.createSoundExpression(WaveShape.Square, 5000, 0, 255, 84, 1000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    basic.showString("" + ("" + game.score()))
     ball.delete()
     item.delete()
     setup()
 }
-buttonClicks.onButtonHeld(buttonClicks.AorB.B, function () {
+
+buttonClicks.onButtonHeld(buttonClicks.AorB.B, function my_function() {
     game.setScore(0)
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("" + (game.score()))
+function point() {
+    
+    game.addScore(1)
+    music.play(music.createSoundExpression(WaveShape.Square, 1, 5000, 198, 164, 200, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+    item.delete()
+    item = game.createSprite(randint(0, 4), randint(0, 4))
+}
+
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    basic.showString("" + ("" + game.score()))
     basic.pause(100)
 })
-function setup () {
+function setup() {
+    
+    sound_on = true
     gameover = false
     started = false
     while (started != true) {
         if (input.buttonIsPressed(Button.A)) {
             timermode = true
             started = true
+            music.play(music.createSoundExpression(WaveShape.Square, 147, 5000, 105, 255, 291, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
             start()
         }
+        
         if (input.buttonIsPressed(Button.B)) {
             timermode = false
             started = true
+            music.play(music.createSoundExpression(WaveShape.Triangle, 1, 5000, 40, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
             start()
         }
+        
         basic.pause(10)
     }
 }
-function start () {
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    
+    if (sound_on) {
+        music.setBuiltInSpeakerEnabled(false)
+        sound_on = false
+    } else {
+        music.setBuiltInSpeakerEnabled(true)
+        sound_on = true
+    }
+    
+})
+function start() {
+    
     ball = game.createSprite(2, 2)
     item = game.createSprite(randint(0, 4), randint(0, 4))
 }
+
 let timer = 0
 let timermode = false
 let started = false
 let gameover = false
-let item: game.LedSprite = null
-let ball: game.LedSprite = null
+let sound_on = false
+let item : game.LedSprite = null
+let ball : game.LedSprite = null
 setup()
-basic.forever(function () {
+basic.forever(function on_forever() {
+    if (!ball.isTouching(item)) {
+        while (!ball.isTouching(item)) {
+            basic.pause(10)
+        }
+        if (ball.isTouching(item)) {
+            point()
+        }
+        
+        basic.pause(10)
+    }
+    
+})
+basic.forever(function on_forever2() {
+    if (input.acceleration(Dimension.X) < -200) {
+        ball.change(LedSpriteProperty.X, -1)
+        basic.pause(50)
+    }
+    
+    if (input.acceleration(Dimension.X) > 200) {
+        ball.change(LedSpriteProperty.X, 1)
+        basic.pause(50)
+    }
+    
+    if (input.acceleration(Dimension.Y) < -200) {
+        ball.change(LedSpriteProperty.Y, -1)
+        basic.pause(50)
+    }
+    
+    if (input.acceleration(Dimension.Y) > 200) {
+        ball.change(LedSpriteProperty.Y, 1)
+        basic.pause(50)
+    }
+    
+    basic.pause(10)
+})
+basic.forever(function on_forever3() {
+    
     if (timermode == true) {
         timer = 0
         while (timer < 30) {
             basic.pause(1000)
             timer += 1
         }
+        basic.pause(10)
         game_over()
     }
-    basic.pause(10)
-})
-basic.forever(function () {
-    if (input.acceleration(Dimension.X) < -200) {
-        ball.change(LedSpriteProperty.X, -1)
-        basic.pause(50)
-    }
-    if (input.acceleration(Dimension.X) > 200) {
-        ball.change(LedSpriteProperty.X, 1)
-        basic.pause(50)
-    }
-    if (input.acceleration(Dimension.Y) < -200) {
-        ball.change(LedSpriteProperty.Y, -1)
-        basic.pause(50)
-    }
-    if (input.acceleration(Dimension.Y) > 200) {
-        ball.change(LedSpriteProperty.Y, 1)
-        basic.pause(50)
-    }
-    basic.pause(10)
-})
-basic.forever(function () {
-    if (!(ball.isTouching(item))) {
-        while (!(ball.isTouching(item))) {
-            basic.pause(10)
-        }
-        if (ball.isTouching(item)) {
-            game.addScore(1)
-            item.delete()
-            item = game.createSprite(randint(0, 4), randint(0, 4))
-        }
-        basic.pause(10)
-    }
+    
 })
