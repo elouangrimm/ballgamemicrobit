f5130205308f69be2224b84368a37157602e97ca
Elouan Grimm <elouan.grimm@gmail.com>
2025-11-21 21:40:12 +0100

    TONS OF CHANGES!!! menu, diagonals...

--- main.ts
+++ main.ts
@@ -1,18 +1,32 @@
+function MainMenu () {
+    basic.clearScreen()
+    if (!(sound_on)) {
+        led.plot(2, 0)
+    } else {
+        led.unplot(2, 0)
+    }
+    menuFlash = !(menuFlash)
+    if (menuFlash) {
+        led.plot(0, 2)
+        led.plot(4, 2)
+        basic.pause(290)
+    } else {
+        led.unplot(0, 2)
+        led.unplot(4, 2)
+        basic.pause(320)
+    }
+}
 buttonClicks.onButtonDoubleClicked(buttonClicks.AorB.A, function () {
 	
 })
 function game_over () {
     basic.clearScreen()
     music.play(music.createSoundExpression(WaveShape.Square, 5000, 0, 255, 84, 1000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-    basic.showString("" + game.score())
+    basic.showString("" + (game.score()))
     ball.delete()
     item.delete()
     setup()
 }
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("" + game.score())
-    basic.pause(10)
-})
 buttonClicks.onButtonHeld(buttonClicks.AorB.B, function () {
     game.setScore(0)
 })
@@ -22,26 +36,37 @@ function point () {
     item.delete()
     item = game.createSprite(randint(0, 4), randint(0, 4))
 }
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (sound_on) {
-        music.setBuiltInSpeakerEnabled(false)
-        music.play(music.createSoundExpression(WaveShape.Triangle, 408, 364, 23, 14, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
-        sound_on = false
-    } else {
-        music.setBuiltInSpeakerEnabled(true)
-        music.play(music.createSoundExpression(WaveShape.Triangle, 408, 540, 255, 230, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
-        sound_on = true
+input.onButtonPressed(Button.AB, function () {
+    if (!(showingScore)) {
+        showingScore = true
+        basic.showString("" + (game.score()))
+        basic.pause(200)
+        showingScore = false
     }
 })
 function setup () {
-    sound_on = true
+    sound_on = false
+    music.setBuiltInSpeakerEnabled(false)
     gameover = false
     started = false
+    MainMenu()
+    if (control.hardwareVersion() == "2") {
+        version = 2
+    } else {
+        version2 = 1
+    }
     while (started != true) {
+        MainMenu()
         if (input.buttonIsPressed(Button.A)) {
             timermode = true
             started = true
             music.play(music.createSoundExpression(WaveShape.Sine, 408, 584, 255, 80, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+            basic.showString("3")
+            basic.pause(75)
+            basic.showString("2")
+            basic.pause(75)
+            basic.showString("1")
+            basic.pause(75)
             start()
         }
         if (input.buttonIsPressed(Button.B)) {
@@ -53,56 +78,85 @@ function setup () {
         basic.pause(10)
     }
 }
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (sound_on) {
+        music.setBuiltInSpeakerEnabled(false)
+        music.play(music.createSoundExpression(WaveShape.Triangle, 408, 364, 23, 14, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+        sound_on = false
+    } else {
+        music.setBuiltInSpeakerEnabled(true)
+        music.play(music.createSoundExpression(WaveShape.Triangle, 408, 540, 255, 230, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+        sound_on = true
+    }
+    if (!(started)) {
+        MainMenu()
+    }
+})
 function start () {
+    basic.clearScreen()
     ball = game.createSprite(2, 2)
     item = game.createSprite(randint(0, 4), randint(0, 4))
 }
-let timer = 0
+let diagDelay = 0
+let moveDelay = 0
+let thresholdDiagonal = 0
+let thresholdStraight = 0
 let timermode = false
+let version2 = 0
+let version = 0
 let started = false
 let gameover = false
-let sound_on = false
+let showingScore = false
 let item: game.LedSprite = null
 let ball: game.LedSprite = null
+let menuFlash = false
+let sound_on = false
+let timer = 0
 setup()
+let sound = images.createImage(`
+    . . # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
-    if (!(ball.isTouching(item))) {
-        while (!(ball.isTouching(item))) {
-            basic.pause(10)
-        }
-        if (ball.isTouching(item)) {
-            point()
-        }
-        basic.pause(10)
+    if (!(started) || showingScore) {
+        return
     }
-})
-basic.forever(function () {
-    if (input.acceleration(Dimension.X) < -200) {
+    if (ball && item && ball.isTouching(item)) {
+        point()
+        basic.pause(100)
+    }
+    thresholdStraight = 200
+    thresholdDiagonal = 120
+    moveDelay = 50
+    diagDelay = Math.round(moveDelay * Math.SQRT2)
+    if (input.acceleration(Dimension.X) < 0 - thresholdStraight && Math.abs(input.acceleration(Dimension.Y)) > thresholdDiagonal) {
         ball.change(LedSpriteProperty.X, -1)
-        basic.pause(50)
-    }
-    if (input.acceleration(Dimension.X) > 200) {
+        ball.change(LedSpriteProperty.Y, input.acceleration(Dimension.Y) < 0 ? -1 : 1)
+        basic.pause(diagDelay)
+    } else if (input.acceleration(Dimension.X) > thresholdStraight && Math.abs(input.acceleration(Dimension.Y)) > thresholdDiagonal) {
         ball.change(LedSpriteProperty.X, 1)
-        basic.pause(50)
-    }
-    if (input.acceleration(Dimension.Y) < -200) {
-        ball.change(LedSpriteProperty.Y, -1)
-        basic.pause(50)
-    }
-    if (input.acceleration(Dimension.Y) > 200) {
-        ball.change(LedSpriteProperty.Y, 1)
-        basic.pause(50)
+        ball.change(LedSpriteProperty.Y, input.acceleration(Dimension.Y) < 0 ? -1 : 1)
+        basic.pause(diagDelay)
+    } else {
+        if (input.acceleration(Dimension.X) < 0 - thresholdStraight) {
+            ball.change(LedSpriteProperty.X, -1)
+            basic.pause(moveDelay)
+        }
+        if (input.acceleration(Dimension.X) > thresholdStraight) {
+            ball.change(LedSpriteProperty.X, 1)
+            basic.pause(moveDelay)
+        }
+        if (input.acceleration(Dimension.Y) < 0 - thresholdStraight) {
+            ball.change(LedSpriteProperty.Y, -1)
+            basic.pause(moveDelay)
+        }
+        if (input.acceleration(Dimension.Y) > thresholdStraight) {
+            ball.change(LedSpriteProperty.Y, 1)
+            basic.pause(moveDelay)
+        }
     }
     basic.pause(10)
-})
-basic.forever(function () {
-    if (timermode == true) {
-        timer = 0
-        while (timer < 30) {
-            basic.pause(1000)
-            timer += 1
-        }
-        basic.pause(10)
-        game_over()
-    }
 })
